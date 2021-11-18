46e739c75da70e15130ba8b2e4d88566ae595bc8
beccamason <beccamason28@gmail.com>
2021-11-18 11:52:41 +0000

    Values available for each account

--- 3SupportBank/Program.cs
+++ 3SupportBank/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace _3SupportBank
 {
@@ -6,7 +9,28 @@ namespace _3SupportBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"C:\Users\Rebecca.Mason\Documents\Corndel\SupportDocs\transactions.txt";
+            string[] transactions = File.ReadAllLines(path);
+
+            Dictionary<string, decimal> accounts = new Dictionary<string, decimal>();
+
+            foreach (string transaction in transactions.Skip(1))
+            {
+                string[] accountData = transaction.Split(",");
+                string fromAccount = accountData[1];
+                string toAccount = accountData[2];
+                decimal value = Convert.ToDecimal(accountData[4]);
+                accounts[fromAccount] = accounts.ContainsKey(fromAccount) ? accounts[fromAccount] - value : 0 - value;
+                accounts[toAccount] = accounts.ContainsKey(toAccount) ? accounts[toAccount] + value : 0 + value;
+            }
+            decimal count = 0;
+            foreach (var pair in accounts)
+            {
+                Console.WriteLine(pair.Key + ", " + pair.Value);
+                count += pair.Value;
+            }
+            Console.WriteLine(count);
+
         }
     }
 }
